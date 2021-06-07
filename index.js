5726731df6496eba16dc8137ea3ba8db6338bc46
withgallantry <matthew.gallantry@gmail.com>
2021-06-07 14:22:29 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-// Run `node index.js` in the terminal
+const maboii = require('maboii');
+const fs = require('fs');
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+// Read keys from file
+let fileBuffer = fs.readFileSync('./keys.bin');
+let keyData = [...fileBuffer];
+console.log(keyData);
+
+const keys = maboii.loadMasterKeys(keyData);
+console.log('Keys', keys);
+
+// let dumpFileBuffer = fs.readFileSync('./text.bin');
+// let unpackResult = maboii.unpack(keys, [...dumpFileBuffer]);
+
+// // If decrypt is successful
+// if (unpackResult.result) {
+//   console.log(unpackResult.unpacked);
+//   // The plain data is available through unpackResult.unpacked
+// }
